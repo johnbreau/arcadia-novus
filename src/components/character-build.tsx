c0fcfda49f1b7badcf2b64c9edf0a16618cc66e1
johnbreau <johnbreau@gmail.com>
2023-12-24 09:07:29 -0600

    dice service added

--- src/components/character-build.tsx
+++ src/components/character-build.tsx
@@ -2,10 +2,13 @@ import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 import { NavBar } from '../components/navbar'
 import { Bookshelf } from '../components/bookshelf'
+import { fourSidedDieFunc } from '../services/dice-service'
 
 import './../styles/character.css'
 
 export const CharacterBuild = () => {
+
+    console.log('gimme a number', fourSidedDieFunc());
 
     return (
         <div className="character">
---
+++ src/services/dice-service.ts
@@ -0,0 +1,36 @@
+
+export const  fourSidedDieFunc = () => {
+    const fourSidedNum =  Math.floor(Math.random() * 4) + 1;
+    console.log('four sided', fourSidedNum);
+    return fourSidedNum;
+  }
+
+  export const sixSidedDieFunc = () => {
+      const sixSidedDie =  Math.floor(Math.random() * 6) + 1;
+      return sixSidedDie;
+  }
+
+  export const eightSidedDieFunc = () => {
+      const eightSidedDie =  Math.floor(Math.random() * 8) + 1;
+      console.log('eight sided', eightSidedDie);
+      return eightSidedDie;
+  }
+
+  export const tenSidedDieFunc = () => {
+      const tenSidedDie =  Math.floor(Math.random() * 10) + 1;
+      console.log('ten sided', tenSidedDie);
+      return tenSidedDie;
+  }
+
+  export const twelveSidedDieFunc = () => {
+      const twelveSidedDie =  Math.floor(Math.random() * 12) + 1;
+      console.log('twelve sided', twelveSidedDie);
+      return twelveSidedDie;
+  }
+
+  export const twentySidedDieFunc = () => {
+      const twentySidedDie =  Math.floor(Math.random() * 20) + 1;
+      console.log('twenty sided', twentySidedDie);
+      return twentySidedDie;
+  }
+
